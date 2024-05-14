2f39941280ee7464fb54a45174f8952dc9f0a898
Sushil Hemrom <sushilhemrom98@gmail.com>
2024-05-14 14:11:35 +0600

    add home page default

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -2,12 +2,17 @@ import { createBrowserRouter } from 'react-router-dom';
 import App from '../App';
 import About from '../pages/dashboard/about/About';
 import Service from '../pages/dashboard/service/Service';
+import Home from '../pages/home/Home';
 
 const router = createBrowserRouter([
     {
         path: '/',
         element: <App />,
         children: [
+            {
+                path:'/',
+                element:<Home />
+            },
             {
                 path:'about',
                 element:<About />
